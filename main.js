a0f16e51c49a16a81d4012cc68cdae2ec25395f1
euni-yi <ejky53@gmail.com>
2022-10-28 10:31:36 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 // HTML Variables
 let gradeEl = document.getElementById("final score")
-let num=0
+let num = 0
 
 
 // Button Event Listener
@@ -11,36 +11,33 @@ document.getElementById("btn").addEventListener("click", btnClicked);
 function btnClicked(){
 
     let answer = document.getElementById("first").value.toLowerCase();
-    
-
     // input
-    if(answer ==="no"){
+    if(answer ==="no"||answer==="incorrect"||answer==="false"){
         console.log("ture")
-        gradeEl.innerHTML = num+1
+        num++
     }
 
     let answer2 = document.getElementById("second").value;
-
     // input
-    if(answer2 ==="3"){
+    if(answer2 ==="3"||answer2==="three"){
         console.log("ture")
-        gradeEl.innerHTML = num+23
     }
 
     let answer3 = document.getElementById("third").value.toLowerCase();
-
     // input
-    if(answer3==="false"){
+    if(answer3==="false"|| answer3==="incorrect" || answer3==="no"){
         console.log("true")
-        gradeEl.innerHTML = num++
+
     }
 
     let answer4 = document.getElementById("fourth").value.toLowerCase();
-
     // input
-    if(answer4==="true"){
+    if(answer4==="true" || answer4==="yes"|| answer4==="correct"){
         console.log("true")
-        gradeEl.innerHTML = num++
+     
+        
     }
+
 }
 
+    
